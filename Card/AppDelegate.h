32384cb64e6b3b294a2a3d7f2e4cb1ae256ea410
Nick <ptnguyen1901@gmail.com>
2020-09-25 12:25:02 -0400

    Initial Commit

---
+++ Card/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Card
+//
+//  Created by Nick Nguyen on 9/25/20.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Card/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  Card
+//
+//  Created by Nick Nguyen on 9/25/20.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
